45cb964cf6c05c7692cdd30549070077dd6c0f72
Dennis W <xiaochou@Dennis-Ws-MacBook-Pro.local>
2024-11-22 15:16:25 -0500

    second commit

--- index.js
+++ index.js
@@ -10,37 +10,19 @@ import EnrollmentsRoutes from './Kanbas/Enrollments/routes.js';
 import "dotenv/config";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
-// Load the correct .env file
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-dotenv.config({ path: envFile });
+
 const app = express();
 
-// Dynamic CORS Configuration
-const allowedOrigins = [
-    "http://localhost:3000", // Local development
-  ];
-
-// Dynamically allow Netlify branch deploy URLs
-if (process.env.NODE_ENV === "production") {
-    allowedOrigins.push(/\.netlify\.app$/); // Match all *.netlify.app subdomains
-  }
 
 app.use(cors(
     {   
         credentials: true,
-        origin: (origin, callback) => {
-      // Allow if no origin (e.g., server-to-server) or origin matches allowedOrigins
-        if (!origin || allowedOrigins.some((o) => (typeof o === "string" ? o === origin : o.test(origin)))) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
     }
 )); 
 
 const sessionOptions = {
-    secret: process.env.production.SESSION_SECRET || "kanbas",
+    secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
